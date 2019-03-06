9fb847c794873f637a4324b1655cdd1ae344ca79
Niek van Leeuwen <niekvleeuwen@hotmail.nl>
2019-03-06 21:02:01 +0100

    Deleted reset button in register form

--- register.php
+++ register.php
@@ -123,7 +123,6 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
             </div>
             <div class="form-group">
                 <input type="submit" class="btn btn-primary" value="Submit">
-                <input type="reset" class="btn btn-default" value="Reset">
             </div>
             <p>Already have an account? <a href="index.php">Login here</a>.</p>
         </form>
