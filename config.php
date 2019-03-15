4a6a99652a9782ddf9a5a8f2c2a9a998ea8a45b5
Niek van Leeuwen <niekvleeuwen@hotmail.nl>
2019-03-15 19:09:21 +0100

    Added transfer form

--- config.php
+++ config.php
@@ -6,10 +6,12 @@ server with default setting (user 'root' with no password) */
 // define('DB_PASSWORD', '44SY33Pm');
 // define('DB_NAME', 'u45644p43373_bank');
 
-define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', 'GlDZ9cbu4FLG');
-define('DB_NAME', 'u45644p43373_bank');
+if(!defined('DB_SERVER')){
+  define('DB_SERVER', 'localhost');
+  define('DB_USERNAME', 'root');
+  define('DB_PASSWORD', 'GlDZ9cbu4FLG');
+  define('DB_NAME', 'u45644p43373_bank');
+}
 
 /* Attempt to connect to MySQL database */
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
--- transfer.php
+++ transfer.php
@@ -8,7 +8,56 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
     exit;
 }
 
-// $_SESSION['username'] = 'Niek'
+// Include config file
+require_once "config.php";
+
+// Define variables and initialize with empty values
+$pin = $amount = $iban = "";
+$pin_err = $amount_err = $iban_err = $stat = $err = "";
+
+// Processing form data when form is submitted
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+
+    // Validate iban
+    if(empty(trim($_POST["iban"]))){
+        $iban_err = "Please enter a IBAN.";
+    } else{
+        include_once "api/functions.php";
+        if(checkiban($_POST["iban"]) !== null){
+          $iban = $_POST['iban'];
+        }else{
+          $iban_err = "Iban is not valid";
+        }
+    }
+
+    // Validate amount
+    if(empty(trim($_POST["amount"]))){
+        $amount_err = "Please enter a amount.";
+    } else{
+      if(is_numeric($_POST["amount"])){
+          $amount= trim($_POST["amount"]);
+      }else{
+          $amount_err = "Please enter numbers as amount";
+      }
+    }
+
+    // Validate pin
+    if(empty(trim($_POST["pin"]))){
+        $pin_err = "Please enter a pin.";
+    } else{
+        if(strlen($_POST["pin"]) == 4){
+          if(is_numeric($_POST["pin"])){
+              $pin = trim($_POST["pin"]);
+          }else{
+              $pin_err = "Please enter numbers as pin";
+          }
+        }else{
+              $pin_err = "Please enter a pin of 4 characters.";
+        }
+    }
+
+    //we need to make a call to the api here
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -23,14 +72,39 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
     <div class="page-header">
         <h1>Hi, <b><?php echo htmlspecialchars($_SESSION["username"]); ?></b>.</h1><p class="text-muted">Welcome to the Monarch Douglas Bank</p>
     </div>
-    <div class="row">
-        <div class="col-sm-3"></div>
-        <div class="col-sm-6">
-            <div class="text-center">
-              <p> Transfer money </p>
+      <div class="row">
+          <div class="center">
+            <div id="addaccount">
+              <div class="col-sm-4"></div>
+              <div class="col-sm-4">
+                <div class="wrapper" align="left">
+                    <h2>Transfer Money</h2>
+                    <p>Please fill in this form to tranfer money.</p>
+                    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+                        <div class="form-group <?php echo (!empty($iban_err)) ? 'has-error' : ''; ?>">
+                            <label>IBAN</label>
+                            <input type="text" name="iban" class="form-control" value="<?php echo $iban; ?>">
+                            <span class="help-block"><?php echo $iban_err; ?></span>
+                        </div>
+                        <div class="form-group <?php echo (!empty($amount_err)) ? 'has-error' : ''; ?>">
+                            <label>Amount</label>
+                            <input type="text" maxlength="10" name="amount" class="form-control" value="<?php echo $amount; ?>">
+                            <span class="help-block"><?php echo $amount_err; ?></span>
+                        </div>
+                        <div class="form-group <?php echo (!empty($pin_err)) ? 'has-error' : ''; ?>">
+                            <label>PIN</label>
+                            <input type="password" maxlength="4" name="pin" class="form-control" value="<?php echo $pin; ?>">
+                            <span class="help-block"><?php echo $pin_err; ?></span>
+                        </div>
+                        <div class="form-group">
+                            <input type="submit" class="btn btn-primary" value="Submit">
+                        </div>
+                    </form>
+                </div>
+              </div>
+              <div class="col-sm-4"></div>
             </div>
-        </div>
-        <div class="col-sm-3"></div>
+          </div>
       </div>
       <br />
       <div class="row">
