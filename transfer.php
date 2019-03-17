41657224085f127b634f222a70e2b8126b9ca3a1
Niek <niekvleeuwen@hotmail.nl>
2019-03-17 22:30:56 +0100

    Edited comment

--- transfer.php
+++ transfer.php
@@ -56,7 +56,37 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
         }
     }
 
+    //get the nuid of the current user
+    $stmt = $link->prepare("SELECT nuid FROM accounts WHERE id = ?");
+    $stmt->bind_param("s", $param_id,);
+    $param_id = $_SESSION["id"];
+
+    if (!$stmt->execute()) {
+        echo(json_encode(array('status' => '1', 'error' => 'Oops! Something went wrong. Please try again later.')));
+        exit();
+    }
+
+    $stmt->bind_result($balance, $iban);
+    $stmt->fetch();
+    $stmt->close();
+
     //we need to make a call to the api here
+    $url = 'api/transfer.php';
+    $data = array('iban' => 'value1', 'key2' => 'value2');
+
+    // use key 'http' even if you send the request to https://...
+    $options = array(
+        'http' => array(
+            'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
+            'method'  => 'POST',
+            'content' => http_build_query($data)
+        )
+    );
+    $context  = stream_context_create($options);
+    $result = file_get_contents($url, false, $context);
+    if ($result === FALSE) { /* Handle error */ }
+
+    var_dump($result);
 }
 ?>
 <!DOCTYPE html>
