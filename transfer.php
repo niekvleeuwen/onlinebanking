0032d578f03d26c86eead07a0dbd1dffb099cd42
Niek <niekvleeuwen@hotmail.nl>
2019-03-17 22:49:01 +0100

    Added api call

--- transfer.php
+++ transfer.php
@@ -22,12 +22,15 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     if(empty(trim($_POST["iban"]))){
         $iban_err = "Please enter a IBAN.";
     } else{
+      $iban = $_POST['iban']; //iban will be checked in api call
+      /*
         include_once "api/functions.php";
         if(checkiban($_POST["iban"]) !== null){
           $iban = $_POST['iban'];
         }else{
           $iban_err = "Iban is not valid";
         }
+      */
     }
 
     // Validate amount
@@ -56,37 +59,43 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
         }
     }
 
-    //get the nuid of the current user
-    $stmt = $link->prepare("SELECT nuid FROM accounts WHERE id = ?");
+    //user is already logged in (so the user is verified and has authorization) so we need to get the pin and nuid of the current user for the api call
+    $stmt = $link->prepare("SELECT pin, nuid FROM accounts WHERE id = ?");
     $stmt->bind_param("s", $param_id,);
     $param_id = $_SESSION["id"];
 
     if (!$stmt->execute()) {
-        echo(json_encode(array('status' => '1', 'error' => 'Oops! Something went wrong. Please try again later.')));
+        echo("Oops! Something went wrong. Please try again later.");
         exit();
     }
 
-    $stmt->bind_result($balance, $iban);
+    $stmt->bind_result($pin, $nuid);
     $stmt->fetch();
     $stmt->close();
 
     //we need to make a call to the api here
-    $url = 'api/transfer.php';
-    $data = array('iban' => 'value1', 'key2' => 'value2');
+    $ch = curl_init();
 
-    // use key 'http' even if you send the request to https://...
-    $options = array(
-        'http' => array(
-            'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
-            'method'  => 'POST',
-            'content' => http_build_query($data)
-        )
-    );
-    $context  = stream_context_create($options);
-    $result = file_get_contents($url, false, $context);
-    if ($result === FALSE) { /* Handle error */ }
+    curl_setopt($ch, CURLOPT_URL,"api/transfer.php");
+    curl_setopt($ch, CURLOPT_POST, 1);
+    curl_setopt($ch, CURLOPT_POSTFIELDS,
+                "nuid=$nuid&pin=$pin&amount=$amount&iban=$iban");
 
-    var_dump($result);
+    // In real life you should use something like:
+    // curl_setopt($ch, CURLOPT_POSTFIELDS,
+    //          http_build_query(array('postvar1' => 'value1')));
+
+    // Receive server response ...
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+    $server_output = curl_exec($ch);
+
+    curl_close ($ch);
+
+    var_dump($server_output);
+
+    /* Further processing ...
+    if ($server_output == "OK") { ... } else { ... } */
 }
 ?>
 <!DOCTYPE html>
